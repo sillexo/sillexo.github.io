8e984c62bb34b1a9757313ce90dc2bab6541db23
Baluev Roman <sillexo@gmail.com>
2021-05-26 12:31:46 +0500

    Правки

--- _nuxt/static/1618309833/services/razrabotka-veb-prilozheniya/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618309833",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices\u002Frazrabotka-veb-prilozheniya",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
